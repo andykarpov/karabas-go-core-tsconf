971fbc0ac1627d552e4601fbc90e4b20e66dcbe8
Andy Karpov <andykarpov@users.noreply.github.com>
2025-09-07 00:18:06 +0200

    reverted audio samplerate gen

--- src/rtl/hdmi/audio_samplerate.v
+++ src/rtl/hdmi/audio_samplerate.v
@@ -9,23 +9,36 @@ parameter CLKRATE = 28000000;
 localparam prescaler = (CLKRATE / SAMPLERATE);
 
 // audio samplerate
-reg stb;
+reg clk_audio;
 reg [9:0] cnt_audio;
 always @(posedge clk, posedge reset)
 begin
-		stb <= 0;
 		if (reset) 
+		begin
 			cnt_audio <= 0;
+			clk_audio <= 0;
+		end
 		else 
 		begin
 			if (cnt_audio > prescaler) 
 			begin
 				cnt_audio <= 0;
-				stb <= 1;
+				clk_audio <= ~clk_audio;
 			end
 			else
 				cnt_audio <= cnt_audio + 1;
 		end
+end
+
+reg stb;
+reg prev_clk_audio;
+always @(posedge clk)
+begin
+	stb <= 0;
+	if (~prev_clk_audio && clk_audio) begin
+		stb <= 1;
+	end
+	prev_clk_audio <= clk_audio;
 end
 
 assign audio_stb = stb;
