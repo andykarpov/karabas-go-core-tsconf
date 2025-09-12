d2cc9c91832593a010570723e413134b64f17dc6
Andy Karpov <andykarpov@users.noreply.github.com>
2025-09-12 10:24:06 +0200

    signal names

--- src/rtl/hdmi/audio_restrober.v
+++ src/rtl/hdmi/audio_restrober.v
@@ -13,31 +13,25 @@ module audio_restrober(
 reg [1:0] stb_reg;
 always @(posedge clk) begin
   if (audio_sample)
-  begin
     stb_reg <= 2'b11;
-  end
   else
     stb_reg <= {stb_reg[0], 1'b0};
 end
 
 // tranfer strobe from clk to clk_ref domain, acquire audio in data reg
-reg [2:0] stb_out_reg;
+reg [2:0] stb_in_reg;
 reg [31:0] audio_in_reg;
-always @(posedge clk_ref)
-begin
-  stb_out_reg <= {stb_out_reg[1:0], stb_reg[1]};   // re-strobe
-  if (stb_out_reg[2:1] == 2'b10) begin // falling edge
+always @(posedge clk_ref) begin
+  stb_in_reg <= {stb_in_reg[1:0], stb_reg[1]};   // re-strobe
+  if (stb_in_reg[2:1] == 2'b10) // falling edge
 	 audio_in_reg <= {audio_l, audio_r};
-  end
 end
 
 // transfer audio in reg into audio out reg
 reg [31:0] audio_out_reg;
-always @(posedge clk)
-begin
-	if (audio_sample) begin
+always @(posedge clk) begin
+	if (audio_sample)
 		audio_out_reg <= audio_in_reg;
-	end
 end
 
 assign {out_l, out_r} = audio_out_reg;
